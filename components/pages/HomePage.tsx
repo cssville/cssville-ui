5556b2dd07ca52f2a783cee4802b60da60dc11df
Evgeniy K <elv1s42k@gmail.com>
2023-11-30 18:14:05 +0200

    ui change

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { Header } from '../ui/Header';
 import { Footer } from '../ui/Footer';
 import { Section } from '../ui/simple/Section';
+import { Column } from '../ui/simple/Column';
 
 export const HomePage = (props) => {
   return (
     <>
       <Header />
       <Section xl>
-
+        <Column>
+          {
+            [
+              ['Home', '/'],
+              ['Buttons', '/button'],
+              ['Typography', '/typography'],
+            ].map(([text, href]) => (
+              <a className="d-flex px-4 py-3 text-decoration-none bg-color-grey-100-hover br-4 color-blue-grey-900" href={href}>
+                <span className="fs-medium fw-bold">{text}</span>
+              </a>
+            ))
+          }
+        </Column>
         <Footer />
       </Section>
     </>
