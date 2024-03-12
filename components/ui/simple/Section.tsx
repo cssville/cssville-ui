f41ec2581d88ad687bcbc8e85fd692d0f5801042
Evgeniy K <elv1s42k@gmail.com>
2024-03-12 17:46:44 +0200

    stack update

--- components/ui/simple/Section.tsx
+++ components/ui/simple/Section.tsx
@@ -7,10 +7,10 @@ export const Section: React.FC<SectionProps> = (props) => buildSimpleComponent(p
   props.fullWidth
     ? ""
     : ['max-w-xs', 'max-w-sm', 'max-w-md', 'max-w-lg', 'max-w-xl'],
-  props.noPadding ? 'p-0' : ['py-2', 'py-3', 'py-4', 'py-5', 'py-6'],
-  props.noPadding ? '' : ['px-2', 'px-3', 'px-4', 'px-5', 'px-6'],
-  props.noPadding ? '' : ['lg-px-1', 'lg-px-2', 'lg-px-3', 'lg-px-4', 'lg-px-5'],
-  props.noPadding ? '' : ['md-px-0', 'md-px-1', 'md-px-2', 'md-px-3', 'md-px-4'],
+  props.noPadding ? 'p-0' : ['py-3', 'py-4', 'py-5', 'py-6', 'py-7'],
+  props.noPadding ? '' : ['px-3', 'px-4', 'px-5', 'px-6', 'px-7'],
+  props.noPadding ? '' : ['lg-px-2', 'lg-px-3', 'lg-px-4', 'lg-px-5', 'lg-px-6'],
+  props.noPadding ? '' : ['md-px-1', 'md-px-2', 'md-px-3', 'md-px-4', 'md-px-5'],
   props.itemsStart
     ? 'align-items-start'
     : props.itemsCenter
